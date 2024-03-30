26e1e69db3d4e3bd261132dcad7e7e8c2c453a7a
Alike73 <alimzhanisla@gmail.com>
2024-03-30 14:25:06 -0400

    U

--- src/components/turntable/TurntableVinyl.js
+++ src/components/turntable/TurntableVinyl.js
@@ -5,14 +5,14 @@ const TurntableVinyl = () => {
     return (
         <div className="vinylOuter">
             <div className="vinylInner">
-                <p className='vinyl_sound_name'>Sound name</p>
+                <p className='vinyl_sound_name m-0'>Sound name</p>
                 <div className="center_pin" />
                 <img 
                     className='melodyLogo' 
                     src={ melodyLogo } 
                     alt="melody logo" 
                 />
-                <p className='vinyl_author_name'>Author</p>
+                <p className='vinyl_author_name m-0'>Author</p>
             </div>
         </div>
     )
