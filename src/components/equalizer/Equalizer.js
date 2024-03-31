d554b79bcdbbfc62c6ebdf06e8b4dfe90fdcfa0f
Alike73 <alimzhanisla@gmail.com>
2024-03-31 00:27:44 -0400

    U

---
+++ src/components/equalizer/Equalizer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Equalizer = () => {
+
+    return (
+        <div id="box" className="animated fadeIn">
+            <div id="circle">
+                <div id="square">
+                    <span id="bar" className="bar2 b1"></span>
+                    <span id="bar" className="bar1 b2"></span>
+                    <span id="bar" className="bar2 b3"></span>
+                    <span id="bar" className="bar1 b4"></span>
+                    <span id="bar" className="bar2 b5"></span>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Equalizer;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Turntable from '../components/turntable/Turntable';
+import Hero from '../components/hero/Hero';
 
 const Main = () => {
 
     return (
         <div>
+            <Hero />
             <Turntable />
         </div>
     )
